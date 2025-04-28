488c3fbbfb4abbf82394b99eb0a99a5707060f01
suj745 <sujsubh2022@gmail.com>
2025-04-28 17:16:23 +0530

    Testing

--- checkeven.py
+++ checkeven.py
@@ -1,5 +1,8 @@
-Number = input("enter the number:")
-if int(Number)%2==0:
-	print("even number")
-else:
-	print("odd number")
+def main():
+	Number = input("enter the number:")
+	if int(Number)%2==0:
+		print("even")
+	else:
+		print("odd number")
+if __name__=="__main__":
+	main()
